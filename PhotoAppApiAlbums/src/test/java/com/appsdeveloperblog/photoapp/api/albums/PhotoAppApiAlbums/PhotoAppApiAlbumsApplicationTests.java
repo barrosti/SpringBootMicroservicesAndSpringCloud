9237e3381270c98c226c8d08c224764731aecd1b
barrosti <barrosti@outlook.com>
2022-04-28 15:46:33 +0100

    initial commit PhotoAppApiAlbums

---
+++ PhotoAppApiAlbums/src/test/java/com/appsdeveloperblog/photoapp/api/albums/PhotoAppApiAlbums/PhotoAppApiAlbumsApplicationTests.java
@@ -0,0 +1,15 @@
+package com.appsdeveloperblog.photoapp.api.albums.PhotoAppApiAlbums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhotoAppApiAlbumsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
